0383a507edfe332765369a31e837c29136a148c5
Adriane <51868027+hairstoa@users.noreply.github.com>
2020-07-01 19:47:31 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,23 @@ document.querySelector("#surprise").href = surpriseLinks[randomNum];
 //Layout for skills
 document.getElementById("hidden1").style.visibility = "hidden";
 document.getElementById("hidden2").style.visibility = "hidden";
+
+// Image Viewer
+// This code was insprired by Traversy Media at https://www.youtube.com/watch?v=afoxd5b0bJo
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".thumbnails img");
+const opacity = 0.4;
+
+imgs.forEach(img => img.addEventListener("click", imgClick));
+
+imgs[0].style.opacity = opacity;
+
+function imgClick(e){
+  // Reset opacity of thumbnails
+  imgs.forEach(img => img.style.opacity = 1);
+  // Change current image to clicked image
+  current.src = e.target.src;
+  // Change transparency of thumbnail
+  e.target.style.opacity = opacity;
+
+}
